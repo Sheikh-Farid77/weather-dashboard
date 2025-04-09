bfbbc654bb9401d773aa0112fb08997903d99c1d
Sheikh Farid <mdfaridsagor101@gmail.com>
2025-04-09 13:08:46 +0600

    jsx markup written complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
+import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 function App() {
- 
-
   return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
+    <div className="grid place-items-center h-screen">
+      <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
